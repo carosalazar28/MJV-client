d85b01a2650e4a5b5b3d43ebe90c0120472604bf
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-03 16:45:56 -0500

    add private routes and also add header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from 'react-router-dom';
 
 import { SignUp } from './pages/SignUp';
+import { Navbar } from './components/Navbar';
+import { Profile } from './pages/Profile';
+
+const token = localStorage.getItem('token');
+
+function PrivateRoute(props) {
+  if(!token) return <Redirect to="/" />;
+
+  return <Route {...props} />;
+}
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Switch>
           <Route exact path="/" component={SignUp} />
+          <PrivateRoute exact path="/profile" component={Profile} />
         </Switch>
       </Router>
     </div>
