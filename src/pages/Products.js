fb018e839807efc5c70461007e5a05598897d1f6
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-04 16:01:35 -0500

    get axios data and map in component product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,49 @@
 import { Product } from '../components/Products/Product';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-export function Products() {
+function Products() {
+
+  const [data, setData] = useState([]);
+  const [errors, setErrors] = useState();
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      baseURL: 'https://jsonplaceholder.typicode.com/',
+      url: 'posts',
+    })
+      .then(({ data}) => setData(data) )
+      .catch(err => {
+        setErrors(err);
+      });
+  }, []);
+
+  const ContainerProducts = styled.section`
+    display: grid;
+    display: grid;
+    grid-template-columns: repeat(3, [col-start] minmax(100px, 1fr) [col-end]);
+    grid-gap: 18px;
+    justify-content: space-around;
+    padding: 15px;
+  `;
+
   return (
     <div>
       <h1>List of Products</h1>
-      <Product/>
+      <ContainerProducts>
+        {!!data && data.length > 0 && data.map(product => (
+          <Product
+            key={product.id}
+            title={product.title}
+            description={product.body}
+            errors={errors}
+          />
+        ))}
+      </ContainerProducts>
     </div>
   );
 }
+
+export default Products;
