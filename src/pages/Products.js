eb6ea90b82069481ca1a6715d2f4cf1f6835d989
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-04 16:47:42 -0500

    fix styles for render the list of products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,15 @@
 import { Product } from '../components/Products/Product';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import { InputForm } from '../components/styled/FormStyles';
+import { MiniSearch } from '../components/MiniSearch';
 
 function Products() {
 
   const [data, setData] = useState([]);
   const [errors, setErrors] = useState();
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios({
@@ -20,6 +23,16 @@ function Products() {
       });
   }, []);
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredProducts = useMemo(() =>
+    data.filter((product) => {
+      return product.title.toLowerCase().includes(search.toLowerCase());
+    }),[data, search]
+  );
+
   const ContainerProducts = styled.section`
     display: grid;
     display: grid;
@@ -29,10 +42,43 @@ function Products() {
     padding: 15px;
   `;
 
+  const ContainerInputSearch = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+
+  const ContainerSearch = styled.div`
+    position: absolute;
+    background-color: white;
+    width: 245px;
+    padding: 0 15px;
+    box-shadow: 0 10px 20px 0 rgb(0 0 0 / 22%);
+    top: 208px;
+  `;
+
   return (
-    <div>
-      <h1>List of Products</h1>
-      <ContainerProducts>
+    <div className="Container__products">
+      <h1 className="Title__products">List of Products</h1>
+      <ContainerInputSearch className="Container__Search">
+        <InputForm
+          type="text"
+          className="Input__search"
+          placeholder="Search a product"
+          value={search}
+          onChange={handleSearch}
+        />
+        <ContainerSearch className="Container__result-search">
+          {search.length > 0 && filteredProducts.map(( { title, id }) => {
+            return (
+              <MiniSearch
+                key={id}
+                title={title}
+              />
+            );
+          })}
+        </ContainerSearch>
+      </ContainerInputSearch>
+      <ContainerProducts className="Container__section-product">
         {!!data && data.length > 0 && data.map(product => (
           <Product
             key={product.id}
