6d45052f57a58b86a1847442f3223442f2198c2e
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-04 16:00:28 -0500

    render the data map for products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,7 +1,20 @@
-export function Product() {
+import {
+  ContainerProduct,
+  Title,
+  Category,
+  Price,
+  Errors
+} from './ProductStyles';
+
+export function Product({ title, description, errors }) {
+
   return (
-    <article>
-      <p>product</p>
-    </article>
+    <ContainerProduct>
+      <Errors>{errors}</Errors>
+      <Title>{title}</Title>
+      <p>{description}</p>
+      <Price>$ 20000</Price>
+      <Category>Varios</Category>
+    </ContainerProduct>
   );
 }
