ed956650688a7d2713b66aa8d68e30cfbcf5fa6d
Carolina <cindy.s.r.28@hotmail.com>
2022-03-07 18:46:20 -0500

    add data fixture

---
+++ cypress/fixtures/user.js
@@ -0,0 +1,6 @@
+const user = {
+  email: 'faker@email.com',
+  password: '12345678',
+};
+
+export default user;
--- cypress/integration/login/singin.spec.js
+++ cypress/integration/login/singin.spec.js
@@ -1,3 +1,5 @@
+import user from '../../fixtures/user';
+
 describe('My First Test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
@@ -8,8 +10,8 @@ describe('My First Test', () => {
   });
 
   it('Should be sign in into app', () => {
-    cy.get('[data-cy=email]').type('test@test.com');
-    cy.get('#password').type('12345678');
+    cy.get('[data-cy=email]').type(user.email);
+    cy.get('#password').type(user.password);
     cy.get('.sc-dlfnbm').click();
 
     cy.get('h1').should('be.visible');
