38d74902a6f284285e21de18518ae3873c89b3c4
Carolina <cindy.s.r.28@hotmail.com>
2022-03-09 20:35:42 -0500

    add intercept request cypress

--- cypress/integration/login/singin.spec.js
+++ cypress/integration/login/singin.spec.js
@@ -1,19 +1,25 @@
 import user from '../../fixtures/user';
 
-describe('My First Test', () => {
+describe('Login Test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
 
-  it('Does not do much!', () => {
+  it('Does not do much', () => {
     expect(true).to.equal(true);
   });
 
   it('Should be sign in into app', () => {
     cy.get('[data-cy=email]').type(user.email);
     cy.get('#password').type(user.password);
-    cy.get('.sc-dlfnbm').click();
+    cy.intercept({
+      method: 'GET',
+      url: '/users/',
+      hostname: 'mjv-db.herokuapp.com',
+    }).as('getProfile');
+    cy.get('[data-cy=submit]').click();
 
-    cy.get('h1').should('be.visible');
+    cy.wait('@getProfile');
+    cy.get('h1').contains('Profile').should('be.visible');
   });
 });
